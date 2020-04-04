9c9fe874777801426619854c4147978770f69d35
Manthan215 <63146258+Manthan215@users.noreply.github.com>
2020-04-04 16:04:24 +0530

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+city="pune","mumbai","calcutta"
+print(city)
+type(city)
+
+
+# In[5]:
+
+
+tuple1=[1,2,3,4]
+list1=[1,2,3,4]
+list1.append(5)
+print(list1)
+
+
+# In[12]:
+
+
+city="bangalore","kolkata","chennai","mumbai"
+print(city)
+print(len(city))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ list (2).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+names=['john','tom','nathan']
+names2=['shreesha','mama']
+print(names)
+print(names2)
+type(names)
+type(names2)
+
+
+# In[11]:
+
+
+subject=['maths','statistics','science']   
+print(subject)  
+print(len (subject))
+type(subject)
+
+
+# In[ ]:
+
+
+
+
