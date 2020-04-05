b5463e581aa57f89e9d4a990ac7c9aacf2346ce1
Manthan215 <63146258+Manthan215@users.noreply.github.com>
2020-04-05 15:19:11 +0530

    Add files via upload

---
+++ input list 1 &2.py
@@ -0,0 +1,18 @@
+
+
+inputlist1=[12,-7,5,64,-14]
+
+print("inputlist1")
+
+new_inputlist1= list(filter(lambda x: x >0, inputlist1))
+
+print("positive numbers in the list:", new_inputlist1)            
+
+
+inputlist2=[12,14,-95,3]
+
+print("inputlist2")
+
+new_inputlist2= list(filter(lambda x: x >0 , inputlist2))
+
+print("positive numbers in the list" , new_inputlist2)
